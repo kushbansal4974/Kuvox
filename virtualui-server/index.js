3a905d302190f5f378331e3e733bcb230d02c8a8
Kush Bansal <bkush535@gmail.com>
2026-07-21 22:26:25 +0530

    authentication of user on server side

--- virtualui-server/index.js
+++ virtualui-server/index.js
@@ -1,14 +1,23 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./configs/connectDB.js"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config()
 
 const app = express()
 
+app.use(express.json())
+app.use(cookieParser())
+
 app.get("/", (req,res)=>{
     res.json("Hello from Server")
 })
+
+// api's
+app.use("/api/auth", authRouter)
+
 
 const PORT = process.env.PORT || 3000
 
