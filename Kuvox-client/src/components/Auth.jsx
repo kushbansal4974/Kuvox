a8ea78b3fb9651bda944aa570edf459f1f999541
Kush Bansal <bkush535@gmail.com>
2026-07-22 18:24:42 +0530

    designing home page

---
+++ Kuvox-client/src/components/Auth.jsx
@@ -0,0 +1,38 @@
+import { AnimatePresence, motion } from "motion/react";
+import { TbX } from "react-icons/tb";
+
+function Auth() {
+  return (
+    <>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="fixed inset-0 flex items-center justify-center bg-black/70 backdrop-blur-sm z-50 p-4"
+        >
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex flex-col sm:flex-row w-full max-w-220 max-h-[90vh] overflow-y-auto rounded-2xl border border-[#3be8ff]/10 shadow-[0_40px_80px_rgba(0,0,0,0.8)] relative"
+          >
+            {/* X button  */}
+            <button className="absolute top-3 right-3 z-20 w-8 h-8 rounded-lg bg-white/5 hover:bg-white/10 flex items-center justify-center text-white/50 hover:text-white transition-all cursor-pointer">
+              <TbX />
+            </button>
+            {/* left box */}
+            <div className="sm:w-[52%] bg-gradient-to-br from-[#03181c] to-[#041e24] p-6 sm:p-10 relative overflow-hidden">
+              <div className="absolute -top-16 -right-16 w-64 h-64 rounded-full bg-[radial-gradient(circle, rgba(59,232,255,0.08)_0%, transparent_70%)] pointer-events-none" />
+            </div>
+
+            {/* right box */}
+            <motion.div></motion.div>
+          </motion.div>
+        </motion.div>
+      </AnimatePresence>
+    </>
+  );
+}
+
+export default Auth;
---
+++ Kuvox-client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Auth from "../components/Auth"
+
+
+function Home() {
+  return (
+    <div className="text-4xl text-red-500">
+      
+      <Auth/>
+    </div>
+  )
+}
+
+export default Home
